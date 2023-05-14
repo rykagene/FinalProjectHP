6c6611674fe8e83dde81b7a54093f9d1271b6392
Ryka Gene Austria <rykastr@gmail.com>
2023-05-14 13:04:07 +0800

    update

--- app/src/main/java/com/example/finalprojecthp/main_characters.java
+++ app/src/main/java/com/example/finalprojecthp/main_characters.java
@@ -2,16 +2,13 @@ package com.example.finalprojecthp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.widget.TextView;
 
-public class main_gryffindor extends AppCompatActivity {
+public class main_characters extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_gryffindor);
-
+        setContentView(R.layout.activity_main_characters);
     }
 }
