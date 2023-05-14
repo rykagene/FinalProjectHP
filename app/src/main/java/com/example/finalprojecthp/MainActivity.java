ed1935c26d781d4db5f743cac5d8b4478276749e
Ryka Gene Austria <rykastr@gmail.com>
2023-05-14 16:06:35 +0800

    update

--- app/src/main/java/com/example/finalprojecthp/MainActivity.java
+++ app/src/main/java/com/example/finalprojecthp/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.finalprojecthp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(getApplicationContext(), main_gryffindor.class);
+        startActivity(intent);
+
     }
 }
