3e26a184ed7ce9005ead894b8b7ada40b99d21d2
Administrator <richardjames.bagay.c@bulsu.edu.ph>
2023-05-14 21:03:07 +0800

    finalized login page

--- app/src/main/java/com/example/finalprojecthp/MainActivity.java
+++ app/src/main/java/com/example/finalprojecthp/MainActivity.java
@@ -5,17 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.ktx.Firebase;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn_signIn;
-
+    EditText email_signIn, password_signIn;
+    Button btn_signIn, btn_signUp;
+    Firebase mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        email_signIn = findViewById(R.id.email_signIn);
+        password_signIn = findViewById(R.id.password_signIn);
         btn_signIn = findViewById(R.id.btn_signIn);
+        btn_signUp = findViewById(R.id.btn_signUp);
 
         btn_signIn.setOnClickListener(v -> {
             Intent intent = new Intent(getApplicationContext(), main_gryffindor.class);
