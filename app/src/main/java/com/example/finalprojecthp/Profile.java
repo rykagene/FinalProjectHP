8f0d80b6a2abc29805b3a147bf4ad27d7e6a50a6
queenyxies <queenycasaclang@gmail.com>
2023-05-17 00:45:01 +0800

    Profile.java

--- app/src/main/java/com/example/finalprojecthp/Profile.java
+++ app/src/main/java/com/example/finalprojecthp/Profile.java
@@ -6,10 +6,17 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Profile extends AppCompatActivity {
 
     Button btnUpdate;
     TextView tvEmail;
+
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+
 
 
 
@@ -20,5 +27,15 @@ public class Profile extends AppCompatActivity {
 
         btnUpdate = findViewById(R.id.btnUpdate);
         tvEmail = findViewById(R.id.tvEmail);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+
+        if (user != null) {
+            String email = user.getEmail();
+            tvEmail.setText(email);
+        }
+
+
     }
 }
